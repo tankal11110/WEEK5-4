74ac03af600480e88adf88c559d830fe6346056e
Tanya Lyakh <Ttanka1110girl@gmail.com>
2017-03-15 17:45:46 +0100

    Finished

--- main.js
+++ main.js
@@ -1,12 +1,80 @@
-if (!String.prototype.repeatt){
-	
-	String.prototype.repeatt = function(count){
-		
-		var new_line = this + " ";
-		
-		for(var i = 1; i < count; i++){
-			new_line += this + " ";
-		}
-		return new_line;
-	}
+function EventEmitter() {
+    this.events = {};
 }
+EventEmitter.prototype.on = function(type, fn) {
+    if(!type || !fn) return;
+	
+    this.events[type] = this.events[type] || [];
+    this.events[type].push(fn);
+}
+EventEmitter.prototype.emit = function(type, data) {
+
+    var fns = this.events[type];
+
+    if(!fns || !fns.length) return;
+
+    for(var i = 0; i < fns.length; i++) {
+        fns[i](data);
+    }
+};
+//-----------------------------------------------------
+function Database(url) {
+    this.url = url;
+	EventEmitter.call(this,{});
+}
+
+Database.prototype.connect = function() {
+
+    // fikcyjne podłączenie do bazy
+    this.emit("connect", this.url);
+
+}
+
+Database.prototype.disconnect = function() {
+
+    // fikcyjne zakończenie połączenia z bazą
+
+    this.emit("disconnect", this.url);
+
+}
+
+Database.prototype = Object.create(EventEmitter.prototype);
+Database.prototype.constructor = Database;
+
+// Użycie EventEmittera
+var ev = new EventEmitter();
+
+ev.on("hello", function(message) {
+    console.log("Witaj " + message + "!");
+});
+
+ev.on("hello", function(message) {
+    console.log("Siema " + message + ".");
+});
+
+ev.on("goodbye", function() {
+    console.log("Do widzenia!");
+});
+
+ev.emit("hello", "Marek");
+ev.emit("goodbye");
+ev.emit("custom"); // nic się nie wydarzy
+
+// DO ZROBIENIA!
+// Docelowe użycie klasy Database
+var db = new Database("db://localhost:3000"); // fikcyjny adres
+
+db.on("connect", function(url) {
+    console.log("Połączenie z bazą pod adresem " + url + " zostało ustanowione.");
+});
+
+db.on("disconnect", function(url) {
+    console.log("Połączenie z bazą pod adresem " + url + " zostało zakończone.");
+});
+
+db.emit("connect");
+
+// po 5 sekundach rozłączamy się
+setTimeout(function() {
+    db.emit("disconnect");
+}, 5000);
