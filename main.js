54eb0109cb630d3cffc1f43a48068ba74bdd2438
Tanya Lyakh <Ttanka1110girl@gmail.com>
2017-03-16 11:13:35 +0100

    Finished

--- main.js
+++ main.js
@@ -1,80 +1,30 @@
-function EventEmitter() {
-    this.events = {};
-}
-EventEmitter.prototype.on = function(type, fn) {
-    if(!type || !fn) return;
+function createData(obj) {
+
+    var data = obj;
+
+	return{
+		
+		set: function(key, value){
+			
+			if (!(key && value)){
+				return;
+			}
+			return data[key] = value;
+			
+		},
+		
+		get: function(key){
+			
+			return data[key];
+			
+		}     
+		
+	}
 	
-    this.events[type] = this.events[type] || [];
-    this.events[type].push(fn);
-}
-EventEmitter.prototype.emit = function(type, data) {
-
-    var fns = this.events[type];
-
-    if(!fns || !fns.length) return;
-
-    for(var i = 0; i < fns.length; i++) {
-        fns[i](data);
-    }
-};
-//-----------------------------------------------------
-function Database(url) {
-    this.url = url;
-	EventEmitter.call(this,{});
 }
 
-Database.prototype.connect = function() {
+var data = createData({});
 
-    // fikcyjne podłączenie do bazy
-    this.emit("connect", this.url);
+data.set("name","Janek");
 
-}
-
-Database.prototype.disconnect = function() {
-
-    // fikcyjne zakończenie połączenia z bazą
-
-    this.emit("disconnect", this.url);
-
-}
-
-Database.prototype = Object.create(EventEmitter.prototype);
-Database.prototype.constructor = Database;
-
-// Użycie EventEmittera
-var ev = new EventEmitter();
-
-ev.on("hello", function(message) {
-    console.log("Witaj " + message + "!");
-});
-
-ev.on("hello", function(message) {
-    console.log("Siema " + message + ".");
-});
-
-ev.on("goodbye", function() {
-    console.log("Do widzenia!");
-});
-
-ev.emit("hello", "Marek");
-ev.emit("goodbye");
-ev.emit("custom"); // nic się nie wydarzy
-
-// DO ZROBIENIA!
-// Docelowe użycie klasy Database
-var db = new Database("db://localhost:3000"); // fikcyjny adres
-
-db.on("connect", function(url) {
-    console.log("Połączenie z bazą pod adresem " + url + " zostało ustanowione.");
-});
-
-db.on("disconnect", function(url) {
-    console.log("Połączenie z bazą pod adresem " + url + " zostało zakończone.");
-});
-
-db.emit("connect");
-
-// po 5 sekundach rozłączamy się
-setTimeout(function() {
-    db.emit("disconnect");
-}, 5000);
+console.log( data.get("name") );
